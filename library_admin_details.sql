776a1286be2ffc23f27703e6a538bc16f641ff80
Sami3160 <110652247+Sami3160@users.noreply.github.com>
2022-10-28 21:23:52 +0530

    these are the databases files

---
+++ library_admin_details.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_details`
+--
+
+DROP TABLE IF EXISTS `admin_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_details` (
+  `username` varchar(10) NOT NULL,
+  `ph_no` decimal(10,0) DEFAULT NULL,
+  `address` varchar(30) DEFAULT NULL,
+  `admin_id` decimal(4,0) NOT NULL,
+  `pass` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `ph_no` (`ph_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_details`
+--
+
+LOCK TABLES `admin_details` WRITE;
+/*!40000 ALTER TABLE `admin_details` DISABLE KEYS */;
+INSERT INTO `admin_details` VALUES ('Atharv',2609090926,'Curch road, Gadhinglaj',2356,'3000'),('Sami',8723498178,'Haidar nagar,Ajra',2938,'1000'),('Malhar',9216871546,'Nana-nani park road,Gadhinglaj',8765,'40000'),('Maaj',3538267776,'Haidar nagar,Ajra',9836,'2000');
+/*!40000 ALTER TABLE `admin_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-28 21:05:44
